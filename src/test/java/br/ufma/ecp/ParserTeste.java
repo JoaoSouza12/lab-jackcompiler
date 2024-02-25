34a67914fc84a25f8aac4b4b93da279b1f9be505
JoaoSouza12 <joao.sousa.12145@gmail.com>
2024-02-25 15:49:26 -0300

    Criação do testParserLess SquareGame() e do testParserWithSquareGame()

--- src/test/java/br/ufma/ecp/ParserTeste.java
+++ src/test/java/br/ufma/ecp/ParserTeste.java
@@ -369,5 +369,31 @@ public class ParserTest extends TestSupport {
 
   }
 
+  @Test
+    public void testParserWithLessSquareGame() throws IOException {
+        var input = fromFile("ExpressionLessSquare/SquareGame.jack");
+        var expectedResult =  fromFile("ExpressionLessSquare/SquareGame.xml");
+
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parse();
+        var result = parser.XMLOutput();
+        expectedResult = expectedResult.replaceAll("  ", "");
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testParserWithSquareGame() throws IOException {
+        var input = fromFile("Square/SquareGame.jack");
+        var expectedResult =  fromFile("Square/SquareGame.xml");
+
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parse();
+        var result = parser.XMLOutput();
+        expectedResult = expectedResult.replaceAll("  ", "");
+        assertEquals(expectedResult, result);
+    }
+
+
+
 
 }
